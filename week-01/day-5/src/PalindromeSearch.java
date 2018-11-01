76e81f6597e85a6f6a96e324352fd9f727425bfc
lyancsie <info@fordit.biz>
2018-11-02 00:01:55 +0100

    BubbleSort done

--- week-01/day-5/src/PalindromeSearch.java
+++ week-01/day-5/src/PalindromeSearch.java
@@ -1,31 +1,28 @@
 public class PalindromeSearch {
     public static void main(String[] args) {
-        String test = "dog goat dad duck doodle never";
-        palindromeSearch(test);
+        String test = "Go ogre reinitialize your rude goon";
+        String[] result = palindromeSearch(test);
+        System.out.println(test);
+        for (int k = 0; k < result.length ; k++) { // minden típusnak megvannak a saját konsansai, metódusai, stb
+            if (result[k] != null) {
+                System.out.println(k + ": " + result[k]);
+            }
+        }
     }
 
     public static String[] palindromeSearch(String input) {
-        String reverseInput = new String();
+        String reverseInput = "";
         for (int i = 1; i <= input.length(); i++) {
-            //      reverseInput.charAt(i) = input.charAt(input.length() - i); Why not good?
+            //      reverseInput.charAt(i) = input.charAt(input.length() - i); Why not good?. A method can NOT be placed to the left side. the .charAt method gives back the value of something
             reverseInput = reverseInput + input.charAt(input.length() - i); //tested, works
         }
-
         System.out.println(reverseInput);
         String output[] = new String[input.length()];
-        for (int n = 3; n < input.length(); n++) { // changes the length the inside for is looking for
-
-            for (int i = 0; i < input.length() - n; i++) {
+        for (int n = 3; n <= input.length(); n++) { // changes the length the inside for is looking for
+            for (int i = 0; i <= input.length() - n; i++) {
                 if (input.substring(i, i + n).equals(reverseInput.substring(reverseInput.length() - n - i, reverseInput.length() - i))) { //Never use "==" in the case of Strings! It is going to be memory reference
                     output[i] = input.substring(i, i + n);
                 }
-            }
-        }
-        for (
-                int k = 0;
-                k < output.length - 1; k++) {
-            if (output[k] != null) {
-                System.out.println(output[k]);
             }
         }
         return output;
