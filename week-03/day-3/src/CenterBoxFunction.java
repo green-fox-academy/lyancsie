1b13dfaf77b2b820528b2b1f93d84202ca6ff49d
lyancsie <info@fordit.biz>
2018-11-07 11:57:38 +0100

    midday commit

--- week-03/day-3/src/CenterBoxFunction.java
+++ week-03/day-3/src/CenterBoxFunction.java
@@ -1,29 +1,28 @@
 import javax.swing.*;
-import java.util.Scanner;
+
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class PositionSquare {
+public class CenterBoxFunction {
 
-    public static void mainDraw(Graphics graphics){
-        // create a square drawing function that takes 2 parameters:
-        // the x and y coordinates of the square's top left corner
-        // and draws a 50x50 square from that point.
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 1 parameter:
+        // the square size
+        // and draws a square of that size to the center of the canvas.
         // draw 3 squares with that function.
         // avoid code duplication.
-        drawRect(graphics);
+        int size=40;
+        graphics.setColor(Color.magenta);
+        for(int i=0; i<3; i++) {
+            squareDraw(size, graphics);
+            size+=20;
+        }
     }
-public static void drawRect(Graphics graphics){
-int x=0; int y=0;
-for(int i = 0; i<3; i++){
-    graphics.setColor(Color.YELLOW);
-    x = (int)(Math.random() * (WIDTH-50));
-    y = (int)(Math.random()* (HEIGHT-50));
-    graphics.fillRect(x,y,50,50);
+    public static void squareDraw(int size, Graphics graphics){
+        graphics.drawRect((WIDTH-size)/2, (HEIGHT-size)/2,size,size);
+    }
 
-}
-}
     // Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 320;
@@ -46,5 +45,4 @@ for(int i = 0; i<3; i++){
             mainDraw(graphics);
         }
     }
-
 }
--- week-03/day-3/src/RainbowBoxFunction.java
+++ week-03/day-3/src/RainbowBoxFunction.java
@@ -4,13 +4,26 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class CenteredSquare {
-
+public class RainbowBoxFunction {
     public static void mainDraw(Graphics graphics) {
-        graphics.setColor(Color.green);
-        graphics.drawRect(WIDTH/2, HEIGHT/2, 10, 10);
-        // Draw a green 10x10 square to the canvas' center.
+        // Create a square drawing function that takes 2 parameters:
+        // The square size, and the fill color,
+        // and draws a square of that size and color to the center of the canvas.
+        // Create a loop that fills the canvas with rainbow colored squares.
+        int size=WIDTH;
+        int R=0; int G=0; int B=0;
+        for (int i = 0; i < WIDTH; i++) {
+            R = (int)(Math.random() * 255);
+            G = (int)(Math.random() * 255);
+            B = (int)(Math.random() * 255);
+            graphics.setColor(new Color(R,G,B));
+            graphics.fillRect((WIDTH-size)/2, (HEIGHT-size)/2,size, size);
+size-=4;
+        }
     }
+public static void draw(Graphics graphics, int size){
+
+}
 
     // Don't touch the code below
     static int WIDTH = 320;
