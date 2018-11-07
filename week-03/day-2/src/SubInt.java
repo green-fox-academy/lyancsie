7765bdda0847af92be304a355e52df094521c2d8
lyancsie <info@fordit.biz>
2018-11-07 18:07:17 +0100

    SubInt.java.added

--- week-03/day-2/src/SubInt.java
+++ week-03/day-2/src/SubInt.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SubInt {
     public static void main(String[] args) {
         //  Create a function that takes a number and a list of numbers as a parameter
@@ -5,21 +8,41 @@ public class SubInt {
         //  Returns an empty list if the number is not part any of the numbers in the list
 
         //  Example:
-        System.out.println(subInt(1, new int[]{1, 11, 34, 52, 61}));
+        System.out.println(subInt(1, new int[]{1, 11, 356215, 318, 11111, 61, 101}));
         //  should print: `[0, 1, 4]`
         System.out.println(subInt(9, new int[]{1, 11, 34, 52, 61}));
         //  should print: '[]'
     }
 
-    public static String[] subInt(int id, int[] list) {
-        String[] output = new String[list.length];
+    public static ArrayList<String> subInt(int id, int[] list) { //not working. the theory itself is mistaken.
         String[] list2 = new String[list.length];
-        for (int i = 0; i < list2.length; i++) {
-            for(int j= 0; j < (list2[j].length()); j++){
-                if(list2[j].equals(id)){}
-            }
-         //   for(int j = 0; j < list.)
+        ArrayList<String> output = new ArrayList<String>();
+        for (int i = 0; i < list2.length; i++) { // copies and converts the integer array to String
+            list2[i] = String.valueOf(list[i]);
+            //System.out.println(list2[i]);
         }
-        return list2;
+        for (int i = 0; i < list2.length; i++) {
+            for (int j = 0; j <= Math.log10(list[i]+1); j++) {
+                if ((String.valueOf(list2[i].charAt(j))).equals(String.valueOf(id))) {
+                    output.add(list2[i]);
+                    break;
+                }
+            }
+        }
+        return output;
+        /*     int digits[] = new int[list.length];
+        ArrayList<Integer> output = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            digits[i] = (int) (Math.log10(list[i]))+1;
+            System.out.println(digits[i]);}
+
+            for (int j = 0; j < list.length; j++) {
+               for (int k = 0; k < digits[j]; k++) {
+                    if(list[k] == id){output.add(j);} */
+
     }
 }
+
+
+
+
