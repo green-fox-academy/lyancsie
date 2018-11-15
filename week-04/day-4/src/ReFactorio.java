a240f54ebc925085a7dcf26f19d0b5812839d240
lyancsie <info@fordit.biz>
2018-11-15 14:57:51 +0100

    refactorio added

---
+++ week-04/day-4/src/ReFactorio.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReFactorio {
+  public static void main(String[] args) {
+
+    System.out.println("Gimmie the number: ");
+    Scanner read = new Scanner(System.in);
+    int n = read.nextInt();
+    System.out.println(reFactorio(n));
+  }
+
+  public static int reFactorio(int n){
+    if(n==1){
+      return n;
+    }
+    else{
+      return n * reFactorio(n-1);
+    }
+  }
+}
