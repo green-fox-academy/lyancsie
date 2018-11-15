0b38d03605cedb28666f3c7712698f6fba3c59c4
lyancsie <info@fordit.biz>
2018-11-15 11:18:02 +0100

    Some other recursion exercises

---
+++ week-04/day-4/src/SumDigit/SumDigit.java
@@ -0,0 +1,25 @@
+package SumDigit;
+
+import java.util.Scanner;
+
+public class SumDigit {
+    public static void main (String[]args){
+      System.out.println("Gimmie the number: ");
+      Scanner read = new Scanner(System.in);
+      int n = read.nextInt();
+      System.out.println(sumDigit(n));
+    }
+
+  private static int sumDigit(int n){
+      int sum = 0;
+      if(n==0) {
+     return n;
+      }
+    else {
+      sum += n % 10; //521 --> 1
+      return (sum + sumDigit( n /10)); // 1 + 2; 1 + 2 + 5
+   }
+  }
+}
+
+
