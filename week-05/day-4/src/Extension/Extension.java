d19d6771e9365c133f7c25ed6ff9ef61c8b172f5
lyancsie <info@fordit.biz>
2018-11-22 14:50:19 +0100

    Extension partly done

---
+++ week-05/day-4/src/Extension/Extension.java
@@ -0,0 +1,48 @@
+package Extension;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by aze on 2017.04.04..
+ */
+public class Extension {
+  
+  int add(int a, int b) {
+    return a + b;
+  }
+  
+  int maxOfThree(int a, int b, int c) {
+    if (a > b) {
+      if (a > c) {
+        return a;
+      } else return c;
+    } else if (c > b) {
+      return c;
+    } else return b;
+  }
+  
+  int median(List<Integer> pool) {
+    Collections.sort(pool);
+    return pool.get((pool.size() - 1) / 2);
+  }
+  
+  boolean isVowel(char c) {
+    return Arrays.asList('a', 'u', 'o', 'e', 'i').contains(c);
+  }
+  
+  String translate(String hungarian) {
+    String teve = hungarian;
+    int length = teve.length();
+    for (int i = 0; i < length; i++) {
+      char c = teve.charAt(i);
+      if (isVowel(c)) {
+        teve = String.join(c + "v" + c, teve.split("" + c));
+        i += 2;
+        length += 2;
+      }
+    }
+    return teve;
+  }
+}
